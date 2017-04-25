39c4b5b61b7b1d7275f307c2635df973c900201b
Alain DEMENET <ademenet@e2r8p16.42.fr>
2017-04-25 16:29:31 +0200

    Update with Vundle install

--- install_myvimrc.sh
+++ install_myvimrc.sh
@@ -4,3 +4,6 @@ curl https://raw.githubusercontent.com/moannuo/myvimrc/master/.vimrc > ~/.vimrc
 
 echo "Installing tmux.conf"
 curl https://raw.githubusercontent.com/moannuo/myvimrc/master/.tmux.conf > ~/.tmux.conf
+
+echo "Installing all Vim's Vundle"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
